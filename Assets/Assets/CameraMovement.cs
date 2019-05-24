4216033529ac6c88e90a7e9be2e891b5f3552507
Qarian <Ferno>
2019-05-24 17:33:30 +0200

    Second Commit

---
+++ Assets/Assets/CameraMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	[SerializeField]
+    Transform player = default;
+	
+	[SerializeField]
+	float cameraSpeed = default;
+	[SerializeField]
+	Vector2 cameraYbounds = default;
+	
+	[SerializeField]
+	KeyCode mouseModeSwitch = default;
+	bool mouseVisible = true;
+	
+	float rotationY;
+	
+	void Start() 
+	{
+		mouseVisible = Cursor.visible;
+		if (mouseVisible)
+			ChangeMouseMode();
+	}
+	
+	void Update()
+	{
+		if (!mouseVisible) {
+			float yAngle = cameraSpeed * Input.GetAxis("Mouse Y");
+			float xAngle = cameraSpeed * Input.GetAxis("Mouse X");
+		
+			player.Rotate(0, xAngle, 0);
+			
+			rotationY = Mathf.Clamp(rotationY + yAngle, cameraYbounds.x, cameraYbounds.y);
+			transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+		}
+		
+		if (Input.GetKeyDown(mouseModeSwitch))
+			ChangeMouseMode();
+			
+	}
+	
+	void ChangeMouseMode()
+	{
+		if (mouseVisible)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		else 
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		mouseVisible = !mouseVisible;
+	}
+}
---
+++ Assets/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	float speed = default;
+	
+	Vector3 moveAxis = Vector3.zero;
+	
+	Rigidbody rb;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+	{
+		moveAxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	}
+	
+    private void FixedUpdate()
+	{
+		Vector3 move = Vector3.zero;
+		move += moveAxis.z * transform.forward;
+		move += moveAxis.x * transform.right;
+		rb.MovePosition(transform.position + move.normalized * speed);
+	}
+}
