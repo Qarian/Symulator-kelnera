2259bc2731723bce0b50c7c080e6140c4ef7da1d
Dawid <dawid.jarmakowski99@gmail.com>
2020-03-20 17:01:15 +0100

    Club Update #6 Particle system is broken

--- Assets/Scripts/Enviromental/FallingPlatformSKS.cs
+++ Assets/Scripts/Enviromental/FallingPlatformSKS.cs
@@ -1,3 +1,52 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:df326f531d3d199f036360ba6e098831fa6a24df6800b25fac7a4d3753527e9e
-size 988
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformSKS : MonoBehaviour
+{
+    [SerializeField] private int jumpToFallPlatform = 10;
+    public static int jumpToFallPlatformStatic;
+
+    void Update()
+    {
+        if(jumpToFallPlatformStatic !=0)
+        {
+            jumpToFallPlatform = jumpToFallPlatformStatic;
+            jumpToFallPlatformStatic = jumpToFallPlatform;
+        }
+        
+        
+    }
+
+    [SerializeField] private float timeToDestroyPlatform = 4f;
+
+    private Rigidbody rb = default;
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.TryGetComponent(out PlayerMovementWallRun player))
+        {
+            if (jumpToFallPlatform > 0)
+            {
+                jumpToFallPlatform--;
+            }
+
+            if (jumpToFallPlatform == 0)
+            {
+                FallPlatform();
+                StartCoroutine(DestroyObject(timeToDestroyPlatform));
+            }
+        }
+    }
+
+    private void FallPlatform()
+    {
+        rb = gameObject.AddComponent<Rigidbody>() as Rigidbody;
+    }
+
+    private IEnumerator DestroyObject(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Gameplay/CheatSystem.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheatSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string cheat;
+    public GameObject inputField;
+    private GameObject search;
+
+    void Start()
+    {
+        search = GameObject.Find("CheatsUI");
+        search.SetActive(!search.activeSelf);
+    }
+
+    public void CheatField()
+    {
+        
+            cheat = inputField.GetComponent<Text>().text;
+
+            switch (cheat)
+            {
+                case "grandma":
+                    Debug.Log("Cheat activated: add money");
+                    GrandmaCheat();
+                    break;
+
+                case "holidays":
+                    Debug.Log("Cheat activated: more time");
+                    HolidaysCheat();
+                    break;
+
+                case "chincha":
+                    Debug.Log("Cheat activated: spawned chinchilla Julian");
+                    SzyszaCheat();
+                    break;
+
+                case "flyhigh":
+                    Debug.Log("Cheat activated: platforms won't fall");
+                    FlyhighCheat();
+                    break;
+
+                case "runboyrun":
+                    Debug.Log("Cheat activated: max speed");
+                    RunboyrunCheat();
+                    break;
+
+                case "ilovebunny":
+                    Debug.Log("Cheat activated: higher jumps");
+                    IlovebunnyCheat();
+                    break;
+
+                case "gohome":
+                    Debug.Log("Cheat activated: day ended");
+                    GohomeCheat();
+                    break;
+
+                case "wuhan":
+                    Debug.Log("To nawet nie jest smieszne...");
+                    break;
+            }
+
+
+            void GrandmaCheat()
+            {
+                //skrypt do dodania pieniedzy
+            }
+
+            void HolidaysCheat()
+            {
+                //skrypt do dodania czasu
+            }
+
+            void SzyszaCheat()
+            {
+                //skrypt do szyszy Julianka
+            }
+
+            void FlyhighCheat()
+            {
+                //skrypt do platform
+                FallingPlatformSKS.jumpToFallPlatformStatic = 10000;
+            }
+
+            void RunboyrunCheat()
+            {
+                //skrypt do biegu
+            }
+
+            void IlovebunnyCheat()
+            {
+                //skrypt do wyzszego skoku
+            }
+
+            void GohomeCheat()
+            {
+                //skrypt do konca dnia
+            }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            search.SetActive(!search.activeSelf);
+            CheatField();
+        }
+            
+    }
+}
+        
+    
+
